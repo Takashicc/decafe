1d50931fff163d4a0d74d52f9d85dc6372cb4ac6
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-03 14:57:06 +0900

    feat: Create owner page

---
+++ src/client/components/Login.tsx
@@ -0,0 +1,132 @@
+import React, { Fragment, useState, useRef } from "react";
+import ReactDOM from "react-dom/client";
+import "../styles/Login.css";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Owner from "./Owner";
+
+interface ErrorMessage {
+  name: string;
+  message: string;
+}
+
+interface FormValue {
+  name: string;
+  pass: string;
+}
+
+function Login() {
+  //seed data
+  const database = [
+    {
+      username: "user1",
+      password: "pass1",
+    },
+    {
+      username: "user2",
+      password: "pass2",
+    },
+  ];
+
+  // React States
+  const [errorMessages, setErrorMessages] = useState<ErrorMessage>({
+    name: "",
+    message: "",
+  });
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+  const [formValue, setFormValue] = useState<FormValue>({
+    name: "",
+    pass: "",
+  });
+
+  // User Login info
+  const loginErrors = {
+    uname: "invalid username",
+    pass: "invalid password",
+  };
+
+  //Update Input
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormValue((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  //Handle Submit
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // Check if username is correct
+    const userData = database.find((user) => user.username === formValue.name);
+
+    // Check if user pass is corect
+    if (userData) {
+      if (userData.password !== formValue.pass) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: loginErrors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "name", message: loginErrors.uname });
+    }
+  };
+
+  // Generate error message
+  const renderErrorMessage = (name: string) => {
+    if (name === errorMessages.name) {
+      return <div className="error">{errorMessages.message}</div>;
+    } else {
+      return undefined;
+    }
+  };
+
+  // Login form to update input
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={formValue.name}
+            required
+          />
+          {renderErrorMessage("name")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input
+            type="password"
+            name="pass"
+            onChange={handleChange}
+            value={formValue.pass}
+            required
+          />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
+  return (
+    <div className="login">
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {isSubmitted ? <Owner /> : renderForm}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/client/components/Owner.tsx
@@ -0,0 +1,7 @@
+import { Fragment } from "react";
+
+const Owner = () => {
+  return <Fragment>This is owner page</Fragment>;
+};
+
+export default Owner;
