9dd8d515440b9094aeb7d36d7119abee6e59723a
Julio Quiezi <julioquiezi@gmail.com>
2022-08-03 15:37:19 +0900

    feat: Add incremental searchbar

--- src/client/components/Searchbar.tsx
+++ src/client/components/Searchbar.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import Select from "react-select";
 import "../styles/searchbar.css";
 
-export default function Header() {
+const Searchbar = () => {
+  const [selectedOptions, setSelectedOptions] = React.useState([]);
+
+  const options = [
+    { label: "Shinjuku", value: 1 },
+    { label: "Shibuya", value: 2 },
+    { label: "Harajuku", value: 3 },
+    { label: "Kyoto", value: 4 },
+    { label: "Osaka", value: 5 },
+  ];
+
   return (
     <div className="searchbarWapper">
-      <input type="search" placeholder="where?" />
-      <button>a button</button>
+      <Select
+        options={options}
+        placeholder="ex. Shinjuku"
+        backspaceRemovesValue
+        isClearable
+        escapeClearsValue
+        closeMenuOnSelect
+        openMenuOnClick={false}
+      />
     </div>
   );
-}
+};
+
+export default Searchbar;
