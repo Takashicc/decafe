4d15fa47338daebce1bcbb8aca5e00ab4078982b
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-03 16:13:16 +0900

    fix: Resolve edits by TL Takashi

---
+++ src/client/components/Createacc.tsx
@@ -0,0 +1,10 @@
+import React, { Fragment, useState, useRef } from "react";
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Createacc: React.FC = () => {
+  return <Fragment>This is create acc page.</Fragment>;
+};
+
+export default Createacc;
