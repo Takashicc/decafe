45fb07b23a8c6c3a347c876321a9236744cb2647
Julio Quiezi <julioquiezi@gmail.com>
2022-08-03 11:43:40 +0900

    feat: Add link for Login component

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-export default function Header () {
-    return (
-        <div className="header">
-            <h1 className="logo">Decafé Logo</h1>
+import "../styles/header.css";
+
+export default function Header() {
+  return (
+    <Router>
+      <div className="header">
+        <div className="picWrapper">
+          <img
+            className="logoPic"
+            width="50"
+            src="https://i.im.ge/2022/08/03/FCRHSS.logo-cafe.jpg"
+            alt="logo cafe"
+          />
         </div>
-    );
+        <div className="shopLinkWrapper">
+          <Link to="/login">Shop Login</Link>
+        </div>
+      </div>
+    </Router>
+  );
 }
+
+//<Route path="/about" component={Login} />
