c593582073545afc75d5fad8078779926361c1b0
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-03 15:27:44 +0900

    fix: resolve reviewed code by TL Takashi

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Login from "./components/Login";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <h1>Hello!</h1>
+    <Login />
   </React.StrictMode>
 );
