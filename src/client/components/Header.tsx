bf045388c64b729294e6dcec9f1295983e679687
Julio Quiezi <julioquiezi@gmail.com>
2022-08-03 10:33:26 +0900

    feat: Create new component Header.tsx

---
+++ src/client/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header () {
+    return (
+        <div className="header">
+            <h1 className="logo">Decafé Logo</h1>
+        </div>
+    );
+}
