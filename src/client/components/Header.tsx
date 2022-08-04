0daf5fb0a9d0c4d197ce3a3ed5ef8a3cd5cd8e2a
Julio Quiezi <julioquiezi@gmail.com>
2022-08-04 18:36:07 +0900

    feat: Add page refresh function when header logo is clicked

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -1,31 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
 import "../styles/header.css";
 
 const Header = () => {
   return (
-    <Router>
-      <div className="header">
-        <div className="picWrapper">
-          <Link to="/">
-            <img
-              className="logoPic"
-              width="50"
-              src="https://i.im.ge/2022/08/03/FCRHSS.logo-cafe.jpg"
-              alt="logo cafe"
-            />
-          </Link>
-        </div>
-        <div className="shopLinkWrapper">
-          <Link to="/login">Shop Login</Link>
-        </div>
+    <div className="header">
+      <div className="picWrapper">
+        <Link to="/" reloadDocument>
+          <img
+            className="logoPic"
+            width="50"
+            src="https://i.im.ge/2022/08/03/FCRHSS.logo-cafe.jpg"
+            alt="logo cafe"
+          />
+        </Link>
       </div>
-    </Router>
+      <div className="shopLinkWrapper">
+        <Link to="/login">Shop Login</Link>
+      </div>
+    </div>
   );
 };
 
 export default Header;
-
-//<Route exact path="/" component={App} />
-//<Route path="/about" component={Login} />
