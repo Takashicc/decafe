008a43b313279c9a031bf65577511fdc9dda7d3c
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-04 11:47:29 +0900

    feat: Create map component

---
+++ src/client/components/Map.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "../styles/Login.css";
+import GoogleMapReact from "google-map-react";
+import "./map.css";
+
+const Map: React.FC = () => {
+  const location = {
+    address: "東京都鴨町６丁目３－１－５５",
+    lat: 35.658304852964726,
+    lng: 139.72788045821426,
+  };
+  return <div></div>;
+};
+
+export default Map;
