2adaeac22758ddb3438f404be3f05df9c628e13d
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-04 17:38:48 +0900

    fix: Fix reviewed portions

--- src/backend/model/data/migrations/20220804075124_add_password_column_to_owner.js
+++ src/backend/model/data/migrations/20220804075124_add_password_column_to_owner.js
@@ -4,8 +4,8 @@ const TABLE_NAME = "owners";
  * @param { import("knex").Knex } knex
  * @returns { Promise<void[]> }
  */
-exports.up = async function (knex) {
-  await knex.schema.alterTable(TABLE_NAME, function (table) {
+exports.up = function (knex) {
+  return knex.schema.alterTable(TABLE_NAME, function (table) {
     table.string("password", 72).notNullable();
   });
 };
@@ -15,7 +15,7 @@ exports.up = async function (knex) {
  * @returns { Promise<void[]> }
  */
 exports.down = function (knex) {
-  knex.schema.alterTable(TABLE_NAME, function (table) {
+  return knex.schema.alterTable(TABLE_NAME, function (table) {
     table.dropColumn("password");
   });
 };
