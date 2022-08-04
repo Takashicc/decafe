32bbd7c706778339f72aa021cd4f2d2436573f13
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-04 16:01:19 +0900

    feat: Add menu component

---
+++ src/client/components/Menu.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "../styles/Login.css";
+
+const Menu: React.FC = () => {
+  const menu = [{ name: "mega-decaf Takashi special", price: "50000" }];
+  return (
+    <div>
+      {menu[0].name}
+      {menu[0].price}
+    </div>
+  );
+};
+
+export default Menu;
