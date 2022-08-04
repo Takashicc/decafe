a38621810ca2024da6cb7eae1f0071a30c41922e
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-04 10:41:52 +0900

    feat: Create shop page

---
+++ src/client/components/Shop.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "../styles/Login.css";
+import { Fragment } from "react";
+import Photoheader from "./Photoheader";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+const Shop: React.FC = () => {
+  return <Photoheader />;
+};
+
+export default Shop;
