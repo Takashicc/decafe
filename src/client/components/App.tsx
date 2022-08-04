b7ccf1e126d1b0383a85a5229e895a406b27ee01
Julio Quiezi <julioquiezi@gmail.com>
2022-08-04 18:30:22 +0900

    feat: Add Home and Login routes to App.tsx

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,11 +1,18 @@
-import Header from "./Header";
-import Searchbar from "./Searchbar";
+import { useState } from "react";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
     <>
-      <Header />
-      <Searchbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home loggedIn={loggedIn} />}></Route>
+          <Route path="/login" element={<div>This is login page</div>}></Route>
+        </Routes>
+      </Router>
     </>
   );
 };
