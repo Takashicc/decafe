71650313e62208a253d3137c44237ce966cc74a5
Julio Quiezi <julioquiezi@gmail.com>
2022-08-05 19:35:33 +0900

    Feat: Display shop info on SearchResult component

--- src/client/components/SearchResult.tsx
+++ src/client/components/SearchResult.tsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllShops } from "../api";
+import { SearchbarOptions } from "./Home";
 
-const SearchResult = ({ selected }: any) => {
+interface AllShopsInfo {
+  name: string;
+  address: string;
+}
+
+interface SearchResultProps {
+  selectedOption: SearchbarOptions | null;
+}
+
+const SearchResult: React.FC<SearchResultProps> = ({ selectedOption }) => {
+  const [allShops, setAllShops] = useState<AllShopsInfo[]>([]);
+
+  let display = allShops.map((shop) => {
+    return (
+      <div className="shopInfo">
+        Shop Name: {shop.name}
+        <br></br>
+        Shop Address: {shop.address}
+      </div>
+    );
+  });
+
+  console.log(allShops);
+
+  //filter shops by city
+  useEffect(() => {
+    (async () => {
+      const result = await getAllShops();
+      const filteredShops: AllShopsInfo[] = result.filter((e) => {
+        return e.city === selectedOption?.label;
+      });
+      setAllShops(filteredShops);
+    })();
+  }, []);
+
   return (
     <div className="shopListWrapper">
       {" "}
-      <h2>Shops in {selected.label}</h2>
-      <div></div>
+      <h2>Shops in {selectedOption?.label}</h2>
+      <div className="shopInfoWrapper">{display}</div>
     </div>
   );
 };
