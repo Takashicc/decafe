39ca9f797aa0e47ff124f600cae081f040227966
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-05 14:57:13 +0900

    feat: Add functionality to use location coords from shop data

--- src/client/components/Map.tsx
+++ src/client/components/Map.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import "../styles/Map.css";
+import modelType from "../model.type";
 
-const Map: React.FC = () => {
-  const location = {
-    address: "東京都鴨町６丁目３－１－５５",
-    lat: 35.658304852964726,
-    lng: 139.72788045821426,
+interface ShopInfoProps {
+  shopDetail: modelType.ShopGet;
+}
+
+const Map: React.FC<ShopInfoProps> = ({ shopDetail }) => {
+  const shoploc = {
+    name: shopDetail?.name,
+    lat: shopDetail?.latitude,
+    lng: shopDetail?.longtitude,
   };
   return (
     <div className="map">
       <iframe
-        src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12966.968358244847!2d${location.lng}!3d${location.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f379344c2681%3A0x26845247535bd152!2sDecafination%20Tokyo!5e0!3m2!1sen!2sjp!4v1659583235913!5m2!1sen!2sjp`}
+        src={`https://maps.google.com/maps?q=${shoploc.lat}, ${shoploc.lng}&z=15&output=embed`}
         width="500"
         height="370"
         loading="lazy"
