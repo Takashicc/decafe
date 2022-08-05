b1f3e2f9f76b490d5fbbfe769c94aa7cbe3f6d01
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-05 14:59:41 +0900

    feat: Add find menus by shop id to menu model

---
+++ src/backend/model/repositories/menu.model.ts
@@ -0,0 +1,48 @@
+import knex from "../knexConfig";
+import { TABLE_MENUS } from "../const";
+import modelType from "../../model.type";
+
+/**
+ * Insert data into shops table.
+ *
+ * @param shop Shop object
+ * @returns Inserted data id
+ */
+export async function createShop(shop: modelType.ShopCreate): Promise<number> {
+  let id: number;
+
+  try {
+    const rows = await knex(TABLE_MENUS).insert(shop, "id");
+    id = rows[0].id;
+  } catch (error) {
+    throw error;
+  }
+
+  return id;
+}
+
+/**
+ * Find menu data by shop id.
+ *
+ * @param id shop id
+ * @returns shop menu find by shop id
+ */
+export async function findMenuById(shopid: number): Promise<modelType.MenuGet> {
+  let menu: modelType.MenuGet[];
+
+  try {
+    menu = await knex
+      .select({
+        name: "name",
+        price: "price",
+      })
+      .from(TABLE_MENUS)
+      .where({
+        shop_id: shopid,
+      });
+  } catch (error) {
+    throw error;
+  }
+
+  return menu;
+}
