e1b3316ae8c09db288822c14de8467845ddb558c
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-05 18:14:07 +0900

    dev: Add test links for dev env

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import ShopDetail from "./components/ShopDetail";
 import ShopMain from "./components/ShopMain";
+import PhotoHeader from "./components/PhotoHeader";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
@@ -10,12 +10,16 @@ const root = ReactDOM.createRoot(
 root.render(
   <>
     <Router>
-      {/* <Link to="/shops/3">Shop 3</Link>
+      <Link to="/shops/1">Shop 1</Link>
+      <Link to="/shops/2">Shop 2</Link>
+      <Link to="/shops/3">Shop 3</Link>
       <Link to="/shops/4">Shop 4</Link>
+      <Link to="/shops/5">Shop 4</Link>
+      <Link to="/shops/6">Shop 4</Link>
+
       <Routes>
-        <Route path="/shops/:id" element={<ShopInfo />} />
-      </Routes> */}
-      <ShopMain />
+        <Route path="/shops/:id" element={<ShopMain />} />
+      </Routes>
     </Router>
   </>
 );
