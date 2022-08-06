9c21397b5d3d3d06d01b3daf216aa4fd7ee5042d
Julio Quiezi <julioquiezi@gmail.com>
2022-08-06 23:01:27 +0900

    style: Add layout for multiple images

--- src/client/components/SearchResult.tsx
+++ src/client/components/SearchResult.tsx
@@ -30,6 +30,22 @@ const SearchResult: React.FC<SearchResultProps> = ({ selectedOption }) => {
                 src="https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
                 alt="cute coffee"
               />
+              <img
+                src="https://images.unsplash.com/photo-1498804103079-a6351b050096?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                alt="cute coffee"
+              />
+              <img
+                src="https://images.unsplash.com/photo-1511081692775-05d0f180a065?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80"
+                alt="cute coffee"
+              />
+              <img
+                src="https://images.unsplash.com/photo-1501339847302-ac426a4a7cbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvZmZlZSUyMHNob3B8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                alt="cute coffee"
+              />
+              <img
+                src="https://images.unsplash.com/photo-1600093463592-8e36ae95ef56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt="cute coffee"
+              />
             </div>
           </div>
         </Link>
@@ -42,7 +58,7 @@ const SearchResult: React.FC<SearchResultProps> = ({ selectedOption }) => {
     (async () => {
       const result = await findAllShops();
       const filteredShops: AllShopsInfo[] = result.filter((e) => {
-        return e.city === selectedOption?.label;
+        return e.city === selectedOption?.value;
       });
       setAllShops(filteredShops);
     })();
@@ -51,7 +67,7 @@ const SearchResult: React.FC<SearchResultProps> = ({ selectedOption }) => {
   return (
     <div className="shopListWrapper">
       <h2>
-        Shops in <span className="cityName">{selectedOption?.label}</span>
+        Shops in <span className="cityName">{selectedOption?.value}</span>
       </h2>
       <div className="shopInfoWrapper">{displayShops}</div>
       <div className="cafeShopPicWrapper"></div>
