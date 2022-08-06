6f25e2375ce196bdafddd64535811cd6a7548851
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-08-06 14:24:56 +0900

    feat: Tell root router to use verify,login,logout,signup

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,8 +1,16 @@
 import express from "express";
+import verifyRouter from "./verifyRouter";
 import shopRouter from "./shopRouter";
+import loginRouter from "./loginRouter";
+import logoutRouter from "./logoutRouter";
+import signUpRouter from "./signUpRoter";
 
 const router = express.Router();
 
+router.use(verifyRouter);
 router.use(shopRouter);
+router.use(loginRouter);
+router.use(logoutRouter);
+router.use(signUpRouter);
 
 export default router;
