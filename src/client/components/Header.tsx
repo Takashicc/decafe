e9c7a26389a962803275da262569b8ccaf7e535b
Thomas Hofmann <thomas.t.hofmann@gmail.com>
2022-08-07 00:31:29 +0900

    feat: Change look of header

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -4,19 +4,9 @@ import "../styles/header.css";
 const Header = () => {
   return (
     <div className="header">
-      <Link to="/" reloadDocument>
-        <div className="picWrapper">
-          <img
-            className="logoPic"
-            width="50"
-            src="https://i.im.ge/2022/08/03/FCRHSS.logo-cafe.jpg"
-            alt="logo cafe"
-          />
-          <div className="decafeText">Decafé</div>
-        </div>
-      </Link>
+      <Link to="/" reloadDocument></Link>
       <div className="shopLinkWrapper">
-        <Link to="/login">Shop Login</Link>
+        <Link to="/login">Cafe Owners</Link>
       </div>
     </div>
   );
