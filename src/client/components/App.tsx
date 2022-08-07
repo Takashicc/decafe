c3f23054e63e646109a5151114634dd089ee4672
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-08-07 22:11:53 +0900

    feat: Fix and add routes

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -2,6 +2,10 @@ import Home from "./Home";
 import OwnerLogin from "./OwnerLogin";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ShopDetail from "./ShopDetail";
+import OwnerSignUp from "./OwnerSignUp";
+import ShopCreate from "./ShopCreate";
+import { PrivateRoute } from "../AuthRouter";
+import OwnerLogout from "./OwnerLogout";
 
 const App = () => {
   return (
@@ -9,7 +13,17 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<OwnerLogin />}></Route>
+          <Route path="/owners/login" element={<OwnerLogin />}></Route>
+          <Route path="/owners/logout" element={<OwnerLogout />}></Route>
+          <Route path="/owners/new" element={<OwnerSignUp />}></Route>
+          <Route
+            path="/shops/new"
+            element={
+              <PrivateRoute>
+                <ShopCreate />
+              </PrivateRoute>
+            }
+          ></Route>
           <Route path="/shops/:id" element={<ShopDetail />}></Route>
         </Routes>
       </Router>
