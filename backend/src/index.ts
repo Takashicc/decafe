17a97d5f95319595261518ca3038d16163682a79
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-10 19:14:10 +0900

    fix: Remove cookie-parser and @types/cookie-parser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,9 @@
 import express from "express";
 import router from "./router";
-import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser());
 
 const corsOptions: cors.CorsOptions = {
   origin: ["http://localhost:3000"],
