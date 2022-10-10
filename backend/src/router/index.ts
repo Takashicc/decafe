fad480a0a6be3d469698d510a65f68348332d32e
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-10 19:08:52 +0900

    refactor: Delete logout router

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import shopRouter from "./shopRouter";
 import menuRouter from "./menuRouter";
 import loginRouter from "./loginRouter";
-import logoutRouter from "./logoutRouter";
 import signUpRouter from "./signUpRouter";
 
 const router = express.Router();
@@ -10,7 +9,6 @@ const router = express.Router();
 router.use(shopRouter);
 router.use(menuRouter);
 router.use(loginRouter);
-router.use(logoutRouter);
 router.use(signUpRouter);
 
 export default router;
