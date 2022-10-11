dc467a0fb9119aea180ef268702f5604194528e2
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-11 20:49:07 +0900

    fix: Read JWT_SECRET_KEY from environment variable

--- backend/src/helper/jwtHelper.ts
+++ backend/src/helper/jwtHelper.ts
@@ -1,12 +1,17 @@
 import { AccessTokenInvalidError } from "../error/Error";
 import * as jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 interface DecodedPayload {
   owner_id: number;
 }
 
 export class jwtHelper {
-  private static SECRET_KEY: jwt.Secret = process.env.SECRET_KEY || "#0sdi0XSd";
+  private static JWT_SECRET_KEY: jwt.Secret =
+    process.env.JWT_SECRET_KEY ||
+    "#9e7fa5d9aa6a30a3eebff43b863a9a33452277d9db4865783d6d615f257bb5a0";
 
   /**
    * Create JsonWebToken.
@@ -20,7 +25,7 @@ export class jwtHelper {
     const options: jwt.SignOptions = {
       expiresIn: "1d",
     };
-    const token = jwt.sign(payload, this.SECRET_KEY, options);
+    const token = jwt.sign(payload, this.JWT_SECRET_KEY, options);
     return token;
   }
 
@@ -32,7 +37,7 @@ export class jwtHelper {
    */
   public static verifyToken(token: string): DecodedPayload {
     try {
-      const decoded = jwt.verify(token, this.SECRET_KEY);
+      const decoded = jwt.verify(token, this.JWT_SECRET_KEY);
       return decoded as DecodedPayload;
     } catch (error) {
       throw new AccessTokenInvalidError();
