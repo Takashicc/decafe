e3905ed08f3454706a95f31a5d54809277d7a51c
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-14 14:51:28 +0900

    refactor: Add new props interface to Map

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,29 +1,25 @@
 import "../styles/Map.css";
-import * as modelType from "model_type";
 
-interface ShopInfoProps {
-  shopDetail: modelType.ShopAndMenu;
+interface MapProps {
+  latitude: number;
+  longitude: number;
+  address: string;
 }
 
-const Map: React.FC<ShopInfoProps> = ({ shopDetail }) => {
-  const shoploc = {
-    lat: shopDetail?.shop?.latitude,
-    lng: shopDetail?.shop?.longitude,
-    address: shopDetail?.shop?.address,
-  };
+const Map = ({ latitude, longitude, address }: MapProps) => {
   return (
     <div className="mapwrapper">
       <span className="location">Location</span>
       <div className="map">
         <iframe
           title="Shop location"
-          src={`https://maps.google.com/maps?q=${shoploc.lat}, ${shoploc.lng}&z=15&output=embed`}
+          src={`https://maps.google.com/maps?q=${latitude}, ${longitude}&z=15&output=embed`}
           width="250"
           height="250"
           loading="lazy"
         ></iframe>
       </div>
-      <div className="address">{shoploc.address}</div>
+      <div className="address">{address}</div>
     </div>
   );
 };
