d6e262d87df7ef5f6e73a63cdb43138b7f6654e2
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-14 14:46:54 +0900

    refactor: Add ShopAndMenus schema

--- frontend/src/api/schema.ts
+++ frontend/src/api/schema.ts
@@ -2,3 +2,16 @@ export interface Auth {
   access_token?: string;
   message?: string;
 }
+
+export interface ShopAndMenus {
+  shop: {
+    name: string;
+    longitude: number;
+    latitude: number;
+    address: string;
+  };
+  menus: {
+    name: string;
+    price: number;
+  }[];
+}
