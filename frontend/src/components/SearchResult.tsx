1b7104ba07da34f5d2cce1af71a1cab6b0e2a0c2
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-14 21:45:30 +0900

    fix, refactor: Fixed a bug that was requesting too many

--- frontend/src/components/SearchResult.tsx
+++ frontend/src/components/SearchResult.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { findAllShops } from "../api";
-import { SearchbarOptions } from "./Home";
 import "../styles/searchResult.css";
 
 interface AllShopsInfo {
@@ -11,49 +10,49 @@ interface AllShopsInfo {
 }
 
 interface SearchResultProps {
-  selectedOption: SearchbarOptions | null;
+  selectedCity: string;
 }
 
-const SearchResult: React.FC<SearchResultProps> = ({ selectedOption }) => {
-  const [allShops, setAllShops] = useState<AllShopsInfo[]>([]);
+const SearchResult = ({ selectedCity }: SearchResultProps) => {
+  const [shops, setShops] = useState<AllShopsInfo[]>([]);
 
-  let displayShops = allShops.map((shop) => {
-    return (
-      <div className="shopInfo">
-        <Link to={"/shops/" + shop.id}>
-          <div className="shopInfoText">
-            <span className="shopName">{shop.name}</span>
-            <br></br>
-            Address: <span className="shopAddress">{shop.address}</span>
-            <div className="imageWrapper">
-              <img
-                src="https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-                alt="cute coffee"
-              />
-            </div>
-          </div>
-        </Link>
-      </div>
-    );
-  });
-
-  //filter shops by city
   useEffect(() => {
     (async () => {
+      // TODO Select shop by city name
       const result = await findAllShops();
       const filteredShops: AllShopsInfo[] = result.filter((e) => {
-        return e.city === selectedOption?.value;
+        return e.city.toLowerCase() === selectedCity.toLowerCase();
       });
-      setAllShops(filteredShops);
+      setShops(filteredShops);
     })();
-  }, [allShops, selectedOption]);
+  }, [selectedCity]);
 
   return (
     <div className="shopListWrapper">
       <h2>
-        Shops in <span className="cityName">{selectedOption?.label}</span>
+        Shops in <span className="cityName">{selectedCity}</span>
       </h2>
-      <div className="shopInfoWrapper">{displayShops}</div>
+      <div className="shopInfoWrapper">
+        {shops.map((shop) => {
+          return (
+            <div className="shopInfo">
+              <Link to={`/shops/${shop.id}`}>
+                <div className="shopInfoText">
+                  <span className="shopName">{shop.name}</span>
+                  <br></br>
+                  Address: <span className="shopAddress">{shop.address}</span>
+                  <div className="imageWrapper">
+                    <img
+                      src="https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                      alt="cute coffee"
+                    />
+                  </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
       <div className="cafeShopPicWrapper"></div>
     </div>
   );
