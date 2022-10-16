6f0fb5629c553b6103e936f8700fd3dd7b2995f9
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-16 17:20:07 +0900

    chore: Add shop detail page

---
+++ frontend/src/pages/shops/[id].tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { findShopAndMenusByShopId } from "@/api";
+import Map from "@/components/Map";
+import Menu from "@/components/Menu";
+import PhotoHeader from "@/components/PhotoHeader";
+import isNumeric from "validator/lib/isNumeric";
+import { ShopAndMenus } from "@/api/schema";
+import { useRouter } from "next/router";
+
+const Detail = () => {
+  const router = useRouter();
+  const id = router.query.id as string;
+  const [shopDetail, setShopDetail] = useState<ShopAndMenus>();
+
+  useEffect(() => {
+    (async () => {
+      if (!id || !isNumeric(id)) {
+        return;
+      }
+      const shopId: number = +id;
+      const shopAndMenus: ShopAndMenus = await findShopAndMenusByShopId(shopId);
+      setShopDetail(shopAndMenus);
+    })();
+  }, [id]);
+
+  const render = () => {
+    if (shopDetail === undefined) {
+      return <></>;
+    }
+
+    return (
+      <div className="Shopmain">
+        <PhotoHeader shopName={shopDetail.shop.name} />
+        <Menu menus={shopDetail.menus} />
+        <Map
+          longitude={shopDetail.shop.longitude}
+          latitude={shopDetail.shop.latitude}
+          address={shopDetail.shop.address}
+        />
+      </div>
+    );
+  };
+
+  return render();
+};
+
+export default Detail;
