a5c1dc02ef98fd57edf44287531b1fba7b156fd2
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-17 13:50:22 +0900

    chore: Make environment variable public to the browser

--- frontend/src/lib/axios_settings.ts
+++ frontend/src/lib/axios_settings.ts
@@ -2,7 +2,7 @@ import { create } from "middleware-axios/dist";
 import Cookies from "js-cookie";
 
 const api = create({
-  baseURL: process.env.BACKEND_BASE_URL || "http://localhost:8080", // TODO Make it environment variable
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080",
 });
 
 api.use(async (config, next, defaults) => {
