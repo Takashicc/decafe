fa335b743cf626e4a3763e3d8cc6f002e49f5999
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-18 16:08:02 +0900

    fix: Enable css on SearchResult

--- frontend/src/components/organisms/SearchResult.tsx
+++ frontend/src/components/organisms/SearchResult.tsx
@@ -1,4 +1,4 @@
-// import "@/styles/searchResult.css";
+import styles from "@/styles/components/organisms/SearchResult.module.scss";
 import { useEffect, useState } from "react";
 import { findAllShops } from "@/api";
 import Link from "next/link";
@@ -28,20 +28,21 @@ const SearchResult = ({ selectedCity }: SearchResultProps) => {
   }, [selectedCity]);
 
   return (
-    <div className="shopListWrapper">
+    <div className={styles.shopListWrapper}>
       <h2>
-        Shops in <span className="cityName">{selectedCity}</span>
+        Shops in <span className={styles.cityName}>{selectedCity}</span>
       </h2>
-      <div className="shopInfoWrapper">
+      <div className={styles.shopInfoWrapper}>
         {shops.map((shop) => {
           return (
-            <div className="shopInfo">
+            <div className={styles.shopInfo}>
               <Link href={`/shops/${shop.id}`}>
-                <div className="shopInfoText">
-                  <span className="shopName">{shop.name}</span>
+                <div className={styles.shopInfoText}>
+                  <span className={styles.shopName}>{shop.name}</span>
                   <br></br>
-                  Address: <span className="shopAddress">{shop.address}</span>
-                  <div className="imageWrapper">
+                  Address:{" "}
+                  <span className={styles.shopAddress}>{shop.address}</span>
+                  <div className={styles.imageWrapper}>
                     <img
                       src="https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
                       alt="cute coffee"
@@ -53,7 +54,7 @@ const SearchResult = ({ selectedCity }: SearchResultProps) => {
           );
         })}
       </div>
-      <div className="cafeShopPicWrapper"></div>
+      <div className={styles.cafeShopPicWrapper}></div>
     </div>
   );
 };
