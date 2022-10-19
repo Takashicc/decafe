3de930a3b25d3c730d40717450f1f4ce94dc9831
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-19 23:22:43 +0900

    feat: Add story for Button atom

---
+++ frontend/src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,47 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Button from "./Button";
+import { CiLogin } from "react-icons/ci";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+
+export default {
+  title: "Design System/Atoms/Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  text: "Login",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  text: "Login",
+  size: "lg",
+};
+
+export const Outline = Template.bind({});
+Outline.args = {
+  text: "Login",
+  variant: "outline",
+};
+
+export const LeftIcon = Template.bind({});
+LeftIcon.args = {
+  text: "Login",
+  leftIcon: <CiLogin />,
+};
+
+export const RightIcon = Template.bind({});
+RightIcon.args = {
+  text: "Login",
+  rightIcon: <CiLogin />,
+};
+
+export const BothIcon = Template.bind({});
+BothIcon.args = {
+  text: "Login",
+  leftIcon: <AiOutlineArrowLeft />,
+  rightIcon: <AiOutlineArrowRight />,
+};
