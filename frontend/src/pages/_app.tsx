61703b89fb8b356057e26b6c2bc10a0b8f591af7
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-19 10:04:42 +0900

    chore: Chakra Provider setup

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import "@/styles/reset.scss";
 import "@/styles/styles.scss";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
